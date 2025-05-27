222f8801c6bc21cf6b91acce3a9ecafa7ca5875b
Vlad-Averyanov <vlad7as@gmail.com>
2025-05-27 11:36:20 +0300

    minor bug fix

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='main'),
-    path('events/', views.EventListView.as_view(), name='event_list'),
+    path('events/', views.EventListView.as_view(), name='events'),
     path('kanban/', views.KanbanView.as_view(), name='kanban'),
     path('create_item/', views.CreateItemView.as_view(), name='create_item'),
     path('edit_item/', views.EditItemView.as_view(), name='edit_item'),
@@ -13,7 +12,7 @@ urlpatterns = [
     path('update_item_status/', views.UpdateItemStatusView.as_view(), name='update_item_status'),
     path('delete_kanban_card/', views.DeleteKanbanCardView.as_view(), name='delete_kanban_card'),
     path('delete_item/', views.DeleteItemView.as_view(), name='delete_item'),
-    path('edit_item_page/<str:item_type>/<int:item_id>/', views.EditItemPageView.as_view(), name='edit_item_page'),
+    path('edit/<str:item_type>/<int:item_id>/', views.EditItemPageView.as_view(), name='edit_item_page'),
     path('get_item/<str:item_type>/<int:item_id>/', views.GetItemView.as_view(), name='get_item'),
     path('get_items/', views.GetItemsView.as_view(), name='get_items'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
